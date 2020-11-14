524953a7dc86ba6745fdd20b12e3944574788f07
Daniel Conceição <danieldcc.colares@gmail.com>
2020-11-14 13:37:52 -0300

    :sparkles: Adiciona a funcão menu

--- Calculadora.js
+++ Calculadora.js
@@ -1,3 +1,36 @@
+let prompt = require('prompt-sync')();
+
+
+menu:
+    while(true != 6){
+       
+        console.log('Calculadora');
+        console.log('Qual operação deseja realizar: ');
+        console.log('1.  Soma');
+        console.log('2.  Subtração');
+        console.log('3.  Multiplicação');
+        console.log('4.  Divisão');
+        console.log('6.  Sair');
+        var opcao = Number(prompt('Opção: '));
+
+            switch(opcao){
+                case 1:
+                    sum();
+                    break
+                case 2:
+                    sub();
+                    break
+                case 3:
+                    mult();
+                    break
+                case 4:
+                    div();
+                    break
+                default:
+                 break menu
+            }
+    }
+
 function sum(){
     //TODO: Implementar soma
 }
